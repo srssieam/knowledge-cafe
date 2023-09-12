ef68a7b8c0607eedb9707dbd31cf09d347c86f4e
srs-sieam <srssieam@gmail.com>
2023-09-12 13:27:21 +0600

    imported useState and pass a function as prop to get bookmarked blog from Blogs.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import BookMarks from './components/BookMarks/BookMarks'
 import Header from './components/Header/Header'
 
 function App() {
+  const [bookmarks, setBookmarks] =useState([]);
+
+  const handleAddToBookmark =(blog)=>{ 
+    console.log('bookmark adding soon')
+  }
 
   return (
     <>
      <Header></Header>
      <main className='md:flex gap-6 mt-7'>
-        <Blogs></Blogs>
+        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>  {/* sending this function as props to get bookmarked blog. */}
         <BookMarks></BookMarks>
      </main >
     </>
